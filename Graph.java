3b9b1ca01535478574056f97268bfa9c2fff8e5d
bryton7 <hayes.bryton@gmail.com>
2018-04-20 16:21:50 -0500

    add graph class

---
+++ Graph.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class Graph{
+		
+		public LinkedList<String> adjList[];
+		public String graphFile;
+		
+		public Graph(String graphData){
+			this.graphFile = graphData;
+		}
+		
+		
+		
+		
+		
+	}
--- NetworkInfluence.java
+++ NetworkInfluence.java
@@ -5,20 +5,7 @@ import java.util.LinkedList;
 
 public class NetworkInfluence{
 	
-	private class Graph{
-		
-		public LinkedList<String> adjList[];
-		public String graphFile;
-		
-		public Graph(String graphData){
-			this.graphFile = graphData;
-		}
-		
-		
-		
-		
-		
-	}
+	
 	
 	
 	
